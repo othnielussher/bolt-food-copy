139e95cf624e3d965e929ac646c319b332fba127
Othniel Ussher <othnielussher16@gmail.com>
2022-02-17 22:14:12 +0000

    Initial UI setup

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:bolt_food/exports.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: const [
+            HeroSection(),
+            Discover(),
+            SignUpSection(),
+            Footer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
