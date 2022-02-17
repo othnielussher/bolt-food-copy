1b5fa108de5b7513a51e1c1ae0c4b01421ec8413
Othniel Ussher <othnielussher16@gmail.com>
2022-02-17 22:14:10 +0000

    Initial UI setup

---
+++ lib/exports.dart
@@ -0,0 +1,15 @@
+// framework
+
+// pub
+export 'package:get/get.dart';
+export 'package:flutter_svg/svg.dart';
+
+// local
+export 'package:bolt_food/home.dart';
+export 'package:bolt_food/constants.dart';
+export 'package:bolt_food/widgets/footer.dart';
+export 'package:bolt_food/widgets/discover.dart';
+export 'package:bolt_food/widgets/hero_section.dart';
+export 'package:bolt_food/widgets/sign_up_card.dart';
+export 'package:bolt_food/models/sign_up_model.dart';
+export 'package:bolt_food/widgets/sign_up_section.dart';
