ed12302fe547862efeeb55c4de75b64dbf955a70
Othniel Ussher <othnielussher16@gmail.com>
2022-02-19 22:52:10 +0000

    Add responsiveness

--- lib/widgets/sign_up_section.dart
+++ lib/widgets/sign_up_section.dart
@@ -6,18 +6,25 @@ class SignUpSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 24.0, right: 24.0, top: 128),
-      alignment: Alignment.center,
-      child: Wrap(
-        spacing: 56,
-        children: List.generate(
-          items.length,
-          (index) {
-            return SignUpCard(signUpModel: items[index]);
-          },
-        ),
-      ),
+    return ResponsiveBuilder(
+      
+      builder: (context, sizingInformation) {
+        bool isDesktop = sizingInformation.isDesktop;
+        return Container(
+          margin: const EdgeInsets.only(left: 24.0, right: 24.0, top: 128),
+          alignment: Alignment.center,
+          child: Wrap(
+            spacing: isDesktop ?  56 : 72,
+            direction: isDesktop ? Axis.horizontal : Axis.vertical,
+            children: List.generate(
+              items.length,
+              (index) {
+                return SignUpCard(signUpModel: items[index]);
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
