899b2c24a979cda6ce64089740c9daf0b26db147
Othniel Ussher <othnielussher16@gmail.com>
2022-02-19 22:52:02 +0000

    Add responsiveness

--- lib/widgets/discover.dart
+++ lib/widgets/discover.dart
@@ -6,30 +6,55 @@ class Discover extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 160.0),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(maxHeight: 848),
-        child: Stack(
-          alignment: Alignment.topLeft,
-          children: [
-            Image.asset(Images.phones),
-            ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 488),
-              child: const Padding(
-                padding: EdgeInsets.only(top: 64, left: 20),
-                child: Text(
-                  'Discover, order, and track in the app',
-                  style: TextStyle(
-                    fontSize: 48,
-                    color: Palette.black,
-                  ),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        bool isDesktop = sizingInformation.isDesktop;
+        return Padding(
+          padding: EdgeInsets.only(top: isDesktop ? 160 : 104),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 848),
+            child: Column(
+              children: [
+                Stack(
+                  alignment: Alignment.topLeft,
+                  children: [
+                    Image.asset(Images.phones),
+                    isDesktop
+                        ? ConstrainedBox(
+                            constraints: const BoxConstraints(maxWidth: 488),
+                            child: const Padding(
+                              padding: EdgeInsets.only(top: 64, left: 20),
+                              child: Text(
+                                'Discover, order, and track in the app',
+                                style: TextStyle(
+                                  fontSize: 48,
+                                  color: Palette.black,
+                                ),
+                              ),
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                  ],
                 ),
-              ),
+                isDesktop
+                    ? const SizedBox.shrink()
+                    : const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 48),
+                        child: Text(
+                          'Discover, order, and track in the app',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            height: 1.4,
+                            fontSize: 24,
+                            color: Palette.black,
+                          ),
+                        ),
+                      ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
