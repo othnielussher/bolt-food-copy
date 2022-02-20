7b2ac7686a9d93e160971aa06e2bd113bff76e73
Othniel Ussher <othnielussher16@gmail.com>
2022-02-20 04:19:36 +0000

    Complete responsiveness

--- lib/widgets/sign_up_section.dart
+++ lib/widgets/sign_up_section.dart
@@ -1,4 +1,4 @@
-import 'package:bolt_food/exports.dart';
+import 'package:bolt_food/shared/exports.dart';
 import 'package:flutter/material.dart';
 
 class SignUpSection extends StatelessWidget {
@@ -7,19 +7,30 @@ class SignUpSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveBuilder(
-      
       builder: (context, sizingInformation) {
         bool isDesktop = sizingInformation.isDesktop;
         return Container(
           margin: const EdgeInsets.only(left: 24.0, right: 24.0, top: 128),
           alignment: Alignment.center,
           child: Wrap(
-            spacing: isDesktop ?  56 : 72,
+            spacing: isDesktop ? 56 : 72,
             direction: isDesktop ? Axis.horizontal : Axis.vertical,
             children: List.generate(
               items.length,
               (index) {
-                return SignUpCard(signUpModel: items[index]);
+                return InkWell(
+                  hoverColor: Colors.transparent,
+                  onTap: () {
+                    if (index == 0) {
+                      Utils.launchURL(Links.couriers);
+                    } else if (index == 1) {
+                      Utils.launchURL(Links.partners);
+                    } else {
+                      Utils.launchURL(Links.careers);
+                    }
+                  },
+                  child: SignUpCard(signUpModel: items[index]),
+                );
               },
             ),
           ),
