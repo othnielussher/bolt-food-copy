8f83970496a8af593ff0904263fdc091ce755f62
Othniel Ussher <othnielussher16@gmail.com>
2022-02-20 04:19:16 +0000

    Complete responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:bolt_food/app.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() => runApp(const App());
+void main() {
+  setPathUrlStrategy();
+  runApp(const App());
+}
