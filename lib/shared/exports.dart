7e3f79fbc9e9578da2bcdfa6536cbd18f3836fa4
Othniel Ussher <othnielussher16@gmail.com>
2022-02-20 04:18:52 +0000

    🚢 Update exports

---
+++ lib/shared/exports.dart
@@ -0,0 +1,18 @@
+// framework
+
+// pub
+export 'package:get/get.dart';
+export 'package:flutter_svg/svg.dart';
+export 'package:responsive_builder/responsive_builder.dart';
+
+// local
+export 'package:bolt_food/home.dart';
+export 'package:bolt_food/shared/utils.dart';
+export 'package:bolt_food/widgets/footer.dart';
+export 'package:bolt_food/shared/constants.dart';
+export 'package:bolt_food/widgets/discover.dart';
+export 'package:bolt_food/widgets/hero_section.dart';
+export 'package:bolt_food/widgets/sign_up_card.dart';
+export 'package:bolt_food/models/sign_up_model.dart';
+export 'package:bolt_food/widgets/sign_up_section.dart';
+export 'package:bolt_food/widgets/language_picker.dart';
